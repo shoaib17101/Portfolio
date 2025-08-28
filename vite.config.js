21381dcef03bdcf8d870167cc14425987a7a8efd
Shoaib <shoaibkhan171015393@gmail.com>
2025-08-28 15:38:50 +0500

    sdf

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/shoaib17101.github.io/Portfolio'
+  base:'/Portfolio'
 })
